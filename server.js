0e16da8aabc0e652dee62263bea84be8b75fae53
ALFAROSO <alberth.sotelo@gmail.com>
2024-06-19 15:17:00 -0700

    update port

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => res.send('<h1>Hello World from Alberth Rodriguez!</h1
 //     res.end('Hello World from Alberth Rodriguez!');
 //     });
 
-app.listen(3000);
-console.log('Server is running on port 3000');
+app.listen(8080);
+console.log('Server is running on port 8080');
